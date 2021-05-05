d4218bca0bb746780553f53c76ceea5f71902581
edinhooliveira <38224332+edinhooliveira@users.noreply.github.com>
2021-05-05 17:20:59 -0300

    add print dos status
    Adicionado funções printRobot e printSensor na função main

--- main.cpp
+++ main.cpp
@@ -6,25 +6,35 @@
 
 using namespace std;
 
+//Protótipo das funções de print
+void printRobot(Simulador *simulador);
+void printSensor(Simulador *simulador);
+
+
 int main(void) {
 	bool dynamicEnvironment = false;
 	int maxGen = 10;
 	int gen = 0;
+	
+	
+		
 	//Simulador::Simulador(int tamX, int tamY, int raio, int posX, int posY, int ang, bool dynamicEnvironment, int maxGen)
 	Simulador *simulador = new Simulador(200, 200, 10, 20, 20, 90, dynamicEnvironment, maxGen);
 	//Simulador *simulador = new Simulador(200,200, dynamicEnvironment, maxGen, gen);
 	
 	std::ofstream myfile;
-	myfile.open ("example.csv");
+	myfile.open ("robot-data.csv");
+	
+	//printRobot(simulador);
+	//printSensor(simulador);
 	
 	cout <<"POSICAO INICIAL" <<endl;
-	cout << "inicial X: " << simulador->getPosX() << endl;
-	cout << "inicial Y: "  << simulador->getPosY() << endl;
-	cout << "inicial Angle: "  << simulador->getAngle() << endl;
+	cout << "Inicial X: " << simulador->getPosX() << endl;
+	cout << "Inicial Y: "  << simulador->getPosY() << endl;
+	cout << "Angulo Inicial: " << simulador->getAngle() << endl;
+	
 	
 	int nrMov  = 67;
-	
-		
 	int *movimentos = new int [nrMov]; //definir passo a passo
 	int *posicoesX = new int [nrMov];
 	int *posicoesY = new int [nrMov];
@@ -32,6 +42,7 @@ int main(void) {
 	double *sensores;
 		
 	/*
+	//Loop para gerar movimentação aleatória do robô
 	for(int i = 0; i < nrMov; i++ ) {
 		acoes[i] = 0;//random_int(0, 4);
 	} */
@@ -55,35 +66,63 @@ int main(void) {
 		
 		sensores = simulador->readSensor(10, gen); //distancia medida sensor = 10
 		
-		for(int j = 0; j < 6; j ++){
-			
+		printRobot(simulador);
+		/*
+		for(int j = 0; j < 6; j ++){	
 			// cout<< "Sensor " << j <<": "<< sensores[j] << endl;
 			cout<< "Sensor " << j <<": "<< *(sensores + j) << endl;
-			
-
+		} */
+		
+		//Adiciona ao arquivo .csv os valores de Posição e angulo do robô
+		myfile << i<<','; //indice
+		myfile << *posicoesX + i<<','; //posicao X
+		myfile << *posicoesY + i<<','; //posicao Y
+		myfile << simulador->getAngle()<<','; //angulo
+		
+		//Adiciona ao arquivo .csv os valores do sensores
+		for(int j = 0; j < 6; j++){
+			myfile << sensores[j]<<',';	
 		}
-		//Valores a serem salvos em arquivo
-		myfile << i<<',';
-		myfile << *posicoesX + i<<',';
-		myfile << *posicoesY + i<<',';
-		myfile << simulador->getAngle()<<',';
-		myfile << sensores[0]<<',';
-		myfile << sensores[1]<<',';
-		myfile << sensores[2]<<',';
-		myfile << sensores[3]<<',';
-		myfile << sensores[4]<<',';
-		myfile << sensores[5]<<',';
-		myfile <<"\n";
-		
-		
+		myfile <<"\n";		
 		
 		cout<< endl;		
 			
 	}
 	myfile.close();
 	
-	
 	cout<<"\tFim do programa!"<<endl;
  	
 	return 0;
 }//main
+
+
+
+
+
+//Funções de Print
+
+void printRobot(Simulador *simulador) {
+		//cout << "Ação: " <<  << endl;
+		//cout << "FUNÇÃO PRINT SIMULADOR" <<endl;
+		cout <<"POSICAO" <<endl;
+		cout << "posicao X: " << simulador->getPosX() << endl;
+		cout << "posicao Y: " << simulador->getPosY() << endl;
+		cout << "getAngle: "  << simulador->getAngle() << endl;
+		cout<< endl;
+	
+}
+
+void printSensor(Simulador *simulador){
+	
+	double *sensores = simulador->readSensor(10,0);
+	
+	//cout << "FUNÇÃO PRINT SENSORES" <<endl;
+	cout <<"SENSORES" <<endl;
+	for(int j = 0; j < 6; j ++){
+		
+		cout<< "Sensor " << j <<": "<< *(sensores + j) << endl;
+	
+	}
+}
+
+
